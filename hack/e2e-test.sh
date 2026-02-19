f8e9c1c28caa39138d1522d0d56adfe5c16c7c73
Matheus Pimenta <matheuscscp@gmail.com>
2026-02-19 18:54:42 +0000

    Enable debug logs in e2e
    Signed-off-by: Matheus Pimenta <matheuscscp@gmail.com>

--- hack/e2e-test.sh
+++ hack/e2e-test.sh
@@ -94,6 +94,7 @@ helm install "$RELEASE_NAME" "$CHART_DIR" \
     --set image.repository="$IMAGE_REPO" \
     --set image.tag="$IMAGE_TAG" \
     --set image.pullPolicy=Never \
+    --set 'podArgs[0]=--log-level=debug' \
     --wait --timeout 120s
 
 log "Waiting for controller deployment to be ready..."
